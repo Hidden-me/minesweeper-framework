8d96429564c9d281b7d80fd9885139c606855fdb
84461810 <431600414@qq.com>
2023-02-09 21:51:42 +0800

    Initial code of "framework": + high-level interface + gradle stuff

---
+++ framework/src/main/java/net/hdme/minesweeper/framework/BoardInfo.java
@@ -0,0 +1,34 @@
+package net.hdme.minesweeper.framework;
+
+/**
+ * Information of a board.
+ */
+public class BoardInfo {
+    /**
+     * The state code of a cell.
+     * 0-8 represents the number of mines around the cell.
+     * <code>CELL_UNKNOWN</code> represents that
+     * the cell has not been discovered.
+     * <code>CELL_FLAG</code> represents that
+     * there is a flag at the cell.
+     * <code>CELL_DEATH</code> represents that
+     * there is a mine at the cell
+     * and the player gets trapped here.
+     */
+    public static final byte
+            CELL_UNKNOWN = 9,
+            CELL_FLAG = 10,
+            CELL_DEATH = 11;
+
+    /**
+     * The state of each cell on the board.
+     */
+    public byte[][] board;
+
+    /**
+     * Whether the player is alive.
+     * This can be inferred from <code>board</code>
+     * and is thus just for saving time.
+     */
+    public boolean alive;
+}
---
+++ framework/src/main/java/net/hdme/minesweeper/framework/MinesweeperCore.java
@@ -0,0 +1,63 @@
+package net.hdme.minesweeper.framework;
+
+/**
+ * A high-level interface of a minesweeper board.
+ * In general, this interface provides the following functions:
+ * <list>
+ *     <li>observing the board</li>
+ *     <li>operating the board</li>
+ * </list>
+ */
+public interface MinesweeperCore {
+
+    // ============
+    // Observations
+    // ============
+
+    /**
+     * Get information of the currently playing board.
+     * See <code>net.hdme.minesweeper.framework.Board</code>
+     * for further info about what a board keeps track of.
+     * @return the playing board information
+     */
+    BoardInfo getBoardInfo();
+
+    // =================
+    // Operations/inputs
+    // =================
+
+    /**
+     * Put a flag at the specified position.
+     * It is typically known as the "right click" operation.
+     * The position is specified by its row number and column number.
+     * @param row the row number
+     * @param col the column number
+     * @throws IllegalArgumentException if the specified position exceeds the border of the board
+     */
+    void flag(int row, int col) throws IllegalArgumentException;
+
+    /**
+     * Try to sweep at the specified position.
+     * It is typically known as the "left click" operation.
+     * The position is specified by its row number and column number.
+     * @param row the row number
+     * @param col the column number
+     * @throws IllegalArgumentException if the specified position exceeds the border of the board
+     */
+    void sweep(int row, int col) throws IllegalArgumentException;
+
+    /**
+     * Try to sweep around the specified position.
+     * It is typically known as the "left-right click" operation.
+     * The position is specified by its row number and column number.
+     * @param row the row number
+     * @param col the column number
+     * @throws IllegalArgumentException if the specified position exceeds the border of the board
+     */
+    void sweepAround(int row, int col) throws IllegalArgumentException;
+
+    /**
+     * Reset the board.
+     */
+    void reset();
+}
